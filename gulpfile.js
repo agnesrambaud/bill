447b976653c4922a69b7a5ce74897635311b0c59
Herve Prot <herve.prot@tomanage.fr>
2016-10-10 13:58:09 +0200

    Add gulp install script

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+        rename = require('gulp-rename'),
         prettify = require('gulp-jsbeautifier'),
         uglify = require('gulp-uglify'),
         cleancss = require('gulp-clean-css'),
@@ -8,19 +9,61 @@ var gulp = require('gulp'),
 
 //copyfiles on root
 gulp.task('copy', function () {
-    var installDirectory = "install/bill";
+    var json = require('./package.json');
 
-    gulp.src([
-        './' + installDirectory + '/**/*',
-        '!./' + installDirectory + '/**/gulpfile.js',
-        '!./' + installDirectory + '/**/LICENSE.md',
-        '!./' + installDirectory + '/**/README.md',
-        '!./' + installDirectory + '/**/package.json',
-        '!./' + installDirectory + '/**/*.zip'])
-            .pipe(gulp.dest('.'));
+    gulp.src('./controllers/*.js')
+            .pipe(rename(function (path) {
+
+                if (path.extname === ".js")
+                    if (path.basename[0] !== '_')
+                        path.basename = "_" + path.basename;
+
+            }))
+            .pipe(gulp.dest('../../controllers/.'));
+
+    gulp.src('./latex/*.tex')
+            .pipe(rename(function (path) {
+
+                if (path.extname === ".tex")
+                    if (path.basename[0] !== '_')
+                        path.basename = "_" + path.basename;
+
+            }))
+            .pipe(gulp.dest('../../latex/.'));
+
+    gulp.src('./app/**/*', {nodir: true})
+            .pipe(rename(function (path) {
+                //console.log(path);
+                var ext = ['.js', '.css'];
+
+                if (ext.indexOf(path.extname) !== -1)
+                    if (path.basename[0] !== '_')
+                        path.basename = "_" + path.basename;
+
+                if (path.extname === ".html")
+                    path.dirname = path.dirname.replace(new RegExp(json.name, 'gi'), "_" + json.name);
+            }))
+            .pipe(gulp.dest('../../app/.'));
+
+    gulp.src('package.json')
+            .pipe(rename(function (path) {
+                path.basename = "_" + json.name;
+
+            }))
+            .pipe(gulp.dest('../../json/.'));
 });
 
+gulp.task('dev', function () {
+    gulp.start('copy');
 
+    return gulp.watch('./**/*', function (obj) {
+
+        if (obj.type === 'changed') {
+            console.log(obj.path, " changed");
+            gulp.start('copy');
+        }
+    });
+});
 
 //-----------------------------------//
 
@@ -51,15 +94,11 @@ gulp.task('minifyhtml', function () {
 
 //task default gulp open help
 gulp.task('readme', function () {
-    var options = {
-        uri: 'localhost:3000',
-        app: 'firefox'
-    };
-    gulp.src('./index.html')
-            .pipe(open(options));
+    gulp.src("./index.html")
+            .pipe(open());
 });
 
 // Tâche par défaut
-//gulp.task('default', ['readme']);
+gulp.task('default', ['readme']);
 gulp.task('allminify', ['minifycss', 'minifyhtml', 'minifyjs']);
 gulp.task('install', ['copy']);
